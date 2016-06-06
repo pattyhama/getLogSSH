3e00d4f5dabce4b543e96d8f9637c0e3ba025b1e
hamaokah <strodr@gmail.com>
2016-06-06 23:21:25 +0900

    initial

---
+++ README.md
@@ -0,0 +1,12 @@
+# Overview
+This is a Python code for obtaining result by executing command via SSH
+
+## Requirement
+Created based on Python 3.5.0
+
+## How to modify code
+1. Import/Download 'project'
+2. ```$ cd 'project-directory'```
+   ```$ source bin/activate```
+   and run as Python 3.5.0 or higher.
+   (Check by ```$ python --version```)
---
+++ ssh_command.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Get result via SSH
+
+import paramiko
+import datetime
+import time
+import cmd
+import sys
+
+hostname = 'node\'s hostname'  # variable
+ipaddress = 'localhost'  # variable
+username = 'user'  # variable
+password = 'pw'  # variable
+date = datetime.date.today().strftime("%Y-%m-%d")
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(ipaddress, username=username, password=password)
+stdin, stdout, stderr = ssh.exec_command('uname -a')
+# print stdout.read()
+
+open_file = stdout.read()
+config_file = open(hostname + "-" + date + ".log", "w")
+config_file.write(open_file)
+config_file.close()
+ssh.close()
+print("Finished python wrote ") + hostname + ("-") + date + (".log")
